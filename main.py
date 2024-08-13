84ae5cc615f186ba85815a7bd58b8b1f655e6614
lcar0029 <lcar0029@student.monash.edu>
2024-08-14 07:52:35 +0800

    Added documentation for main.py file

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 from pydantic import BaseModel
-from typing import List
-import parameters.locations as loc
-import parameters.forecast_type as type
-import parameters.variables as var
+import bodyParameters.locations as loc
+import bodyParameters.forecast_type as type
+import bodyParameters.variables as var
 
 app = FastAPI()
 
-class forecastBody(BaseModel):
-    location: str
-    forecastType: str
-    variables: list[str]
+"""
+Get a list of valid locations. This includes cities and towns.
 
+Returns:
+    {
+        "locations": list[str]
+    }
+"""
 @app.get("/locations")
 async def getLocations():
     res = {
@@ -19,6 +21,14 @@ async def getLocations():
     }
     return res
 
+"""
+Get a list of valid forecast durations.
+
+Returns:
+    {
+        "locations": list[str]
+    }
+"""
 @app.get("/forecastTypes")
 async def getForecastTypes():
     res = {
@@ -26,6 +36,14 @@ async def getForecastTypes():
     }
     return res
 
+"""
+Get a list of valid weather variables.
+
+Returns:
+    {
+        "locations": list[str]
+    }
+"""
 @app.get("/variables")
 async def getVariables():
     res = {
@@ -33,12 +51,36 @@ async def getVariables():
     }
     return res
 
+class forecastBody(BaseModel):
+    location: str
+    forecastType: str
+    variables: list[str]
+    
+"""
+Get the forecast of a location.
+
+Arguments:
+    {
+        "location": str
+        "forecastType": str
+        "variables": list[str]
+    }
+
+Returns:
+    {
+        "success": bool,
+        "temperature": (optional) list[float],
+        "humidity": (optional) list[float],
+        "precipitation": (optional) list[float],
+    } 
+"""    
 @app.get("/forecast")
 async def getForecast(body: forecastBody):
     res = {
         "success": False
     }
     
+    # Check if request body is valid
     isValidLocation = body.location in loc.Locations().getLocations()
     isValidForecastType = body.forecastType in type.ForecastType().getTypes()
     isValidVariables = all(item in var.Variables().getVariables() for item in body.variables)
@@ -51,11 +93,13 @@ async def getForecast(body: forecastBody):
         "precipitation": ["0.0", "0.9", "1.0", "0.5", "0.8"]
     }
     
+    # Invalid request body
     if (not isValidLocation \
         or not isValidForecastType \
         or not isValidVariables):
         return res
     
+    # Return requested variables
     for v in body.variables:
         res[v] = mock_res[v]
     res["success"] = True
