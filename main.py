38135c1f754958bc87a2b35014263567e22c58b3
lcar0029 <lcar0029@student.monash.edu>
2024-08-14 07:41:29 +0800

    Added variables to GET request for /forecast

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
+from typing import List
 import parameters.locations as loc
 import parameters.forecast_type as type
 import parameters.variables as var
@@ -9,6 +10,7 @@ app = FastAPI()
 class forecastBody(BaseModel):
     location: str
     forecastType: str
+    variables: list[str]
 
 @app.get("/locations")
 async def getLocations():
@@ -33,7 +35,30 @@ async def getVariables():
 
 @app.get("/forecast")
 async def getForecast(body: forecastBody):
-    if (body.location in loc.Locations().getLocations() \
-        and body.forecastType in type.ForecastType().getTypes()):
-        return {"success": True}
-    return {"success": False, "reason": "Invalid location or forecastType provided."}
+    res = {
+        "success": False
+    }
+    
+    isValidLocation = body.location in loc.Locations().getLocations()
+    isValidForecastType = body.forecastType in type.ForecastType().getTypes()
+    isValidVariables = all(item in var.Variables().getVariables() for item in body.variables)
+    
+    # TODO: Hardcoded variables returned for now, replace with models
+    # TODO: Filter hourly results to only return forecasts after current time
+    mock_res = {
+        "temperature": ["25.5", "26.3", "26.1", "26.0", "26.3"],
+        "humidity": ["80.1", "77.8", "77.8", "77.7", "83.0"],
+        "precipitation": ["0.0", "0.9", "1.0", "0.5", "0.8"]
+    }
+    
+    if (not isValidLocation \
+        or not isValidForecastType \
+        or not isValidVariables):
+        return res
+    
+    for v in body.variables:
+        res[v] = mock_res[v]
+    res["success"] = True
+    
+    return res
+        
