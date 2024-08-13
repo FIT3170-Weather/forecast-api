b90f9f1c86b9ccc8d8ea98b750751dc7cef082c3
lcar0029 <lcar0029@student.monash.edu>
2024-08-14 07:03:36 +0800

    Added endpoint to get forecast

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 from fastapi import FastAPI, File, UploadFile
 from pydantic import BaseModel
-import locations.locations as loc
+import parameters.locations as loc
+import parameters.forecast_type as type
 
 app = FastAPI()
 
+class forecastBody(BaseModel):
+    location: str
+    forecastType: str
+
 @app.get("/locations")
-async def hourly():
+async def getLocations():
     return loc.Locations().getLocations()
+
+@app.get("/forecastTypes")
+async def getForecastTypes():
+    return type.ForecastType().getTypes()
+
+@app.get("/forecast")
+async def getForecast(body: forecastBody):
+    if (body.location in loc.Locations().getLocations() \
+        and body.forecastType in type.ForecastType().getTypes()):
+        return {"success": True}
+    return {"success": False, "reason": "Invalid location or forecastType provided."}
---
+++ parameters/forecast_type.py
@@ -0,0 +1,9 @@
+class ForecastType:
+    def __init__(self):
+        self.types = [
+            "hourly",
+            "daily"
+            ]
+        
+    def getTypes(self):
+        return self.types
