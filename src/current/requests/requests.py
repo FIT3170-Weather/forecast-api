97b1975b80436b98f09ac2e0f83aba414da301b2
lcar0029 <lcar0029@student.monash.edu>
2024-08-14 10:40:00 +0800

    Added GET request to get current weather data from OpenWeatherMap's API

--- src/current/requests/requests.py
+++ src/current/requests/requests.py
@@ -1,8 +1,9 @@
 # from main import app
 from fastapi import APIRouter
 from pydantic import BaseModel
+import requests
 import src.forecast.bodyParameters.locations as loc
-# from apiKey import API_KEY
+from .apiKey import API_KEY
 
 router = APIRouter()
 
@@ -15,6 +16,11 @@ async def getCurrentWeather(body: currentBody):
     isValidLocation = body.location in loc.Locations().getLocations()
     
     if isValidLocation:
-        return {"success": True}
+        lat, lon = 3.1319, 101.6841
+        url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
+        
+        res = requests.get(url)
+        if res.status_code == 200:
+            return res.json()
     
     return {"success": False}
