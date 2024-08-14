34eebd03065ddf53c56ae93c1c0e0ed8f08a27ad
lcar0029 <lcar0029@student.monash.edu>
2024-08-14 11:27:02 +0800

    Removed unused code from src/main.py

--- src/main.py
+++ src/main.py
@@ -9,16 +9,3 @@ app = FastAPI() # "uvicorn main:app --reload" to run on localhost:8000
 
 app.include_router(current.router)
 app.include_router(forecast.router)
-
-# class currentBody(BaseModel):
-#     location: str
-    
-# @app.get("/current")
-# async def getCurrentWeather(body: currentBody):
-    
-#     isValidLocation = body.location in loc.Locations().getLocations()
-    
-#     if isValidLocation:
-#         return {"success": True}
-    
-#     return {"success": False}
