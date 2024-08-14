01fe958b869e6caa3f65d36156bde8ab78e63dc9
lcar0029 <lcar0029@student.monash.edu>
2024-08-14 11:26:10 +0800

    Add documentation for /current endpoint

--- src/current/requests/requests.py
+++ src/current/requests/requests.py
@@ -1,4 +1,3 @@
-# from main import app
 from fastapi import APIRouter
 from pydantic import BaseModel
 import requests
@@ -8,15 +7,28 @@ from .apiKey import API_KEY
 router = APIRouter()
 
 class currentBody(BaseModel):
-    location: str
+    location: str   # as defined in locations.py
+
+"""
+Gets the current weather variables from one location.
+
+Arguments:
+    {
+        "location": string (as defined in locations.py or GET /locations endpoint)
+    }
     
+Return:
+    See: https://openweathermap.org/current#example_JSON
+"""    
 @router.get("/current")
 async def getCurrentWeather(body: currentBody):
     
     isValidLocation = body.location in loc.Locations().getLocations()
     
     if isValidLocation:
-        lat, lon = 3.1319, 101.6841
+        # Get coordinates of location
+        coord = loc.Locations().getLocations()[body.location]
+        lat, lon = coord["lat"], coord["lon"]
         url = f"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API_KEY}"
         
         res = requests.get(url)
