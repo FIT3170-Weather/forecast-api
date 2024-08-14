acdb96748bceb9f0009d9330750e74d974329a7d
lcar0029 <lcar0029@student.monash.edu>
2024-08-14 11:29:02 +0800

    Added in-line comments as instructions on how to start the backend on localhost

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import uvicorn
 
+# Run 'python main.py' to start on localhost
 if __name__ == "__main__":
     uvicorn.run("src.main:app", host="127.0.0.1", port=8000, reload=True)
--- src/main.py
+++ src/main.py
@@ -1,11 +1,10 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import src.forecast.bodyParameters.locations as loc
-# from apiKey import API_KEY
 from .current.requests import requests as current
 from .forecast.requests import requests as forecast
 
-app = FastAPI() # "uvicorn main:app --reload" to run on localhost:8000
+app = FastAPI() # DO NOT RUN FROM HERE; run /main.py instead
 
 app.include_router(current.router)
 app.include_router(forecast.router)
