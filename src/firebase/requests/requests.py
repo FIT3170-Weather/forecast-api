4ead786e6aec3d7a0a7e7e37cc3071dfa279dd66
Daryl <dlim0036@student.monash.edu>
2024-08-15 20:22:49 +0800

    Refactored code by separting the responses classes and implemented the GET request to get preferences for the user

--- src/firebase/requests/requests.py
+++ src/firebase/requests/requests.py
@@ -1,10 +1,8 @@
-import json
-from typing import Dict, List, Union
 import firebase_admin
 from firebase_admin import firestore
 from firebase_admin import credentials
 from fastapi import APIRouter
-from pydantic import BaseModel
+from src.firebase.response_models.responses import *
 
 
 cred = credentials.Certificate("serviceAccountKey.json")
@@ -12,34 +10,6 @@ firebase_admin.initialize_app(cred)
 
 db = firestore.client()
 router = APIRouter()
-
-class Alerts(BaseModel):
-    thunder: bool
-    rain: bool
-
-class Preferences(BaseModel):
-    locations: List[str]
-
-class ProfileData(BaseModel):
-    email: str
-    password: str
-    username: str
-
-class ProfileDocument(BaseModel):
-    uid: str
-    alerts: Alerts
-    preferences: Preferences
-    profile_data: ProfileData
-
-class Response(BaseModel):
-    status: str
-    data: Union[List[ProfileDocument], None] = None
-    error: Union[str, None] = None
-
-class AlertsResponse(BaseModel):
-    status: str
-    data: Alerts
-    error: Union[str, None] = None
 
 @router.get("/profiles", response_model=Response)
 async def getProfiles():
@@ -113,4 +83,27 @@ async def getAlerts(uid: str):
     
     except Exception as e:
         return AlertsResponse(status="error", error=f"An error occurred: {str(e)}")
+    
+@router.get("/profiles/{uid}/preferences", response_model=PreferencesResponse)
+async def getPreferences(uid: str):
+    try:
+        # Reference to the "profiles" collection
+        profile_ref = db.collection("profiles").document(uid)
+        
+        # Get the document snapshot
+        profile = profile_ref.get()
+        
+        
+        # Check if the document exists
+        if profile.exists:
+            # Reference to the "alerts" subcollection
+            data_dict = profile.to_dict()
+            res = Preferences(**data_dict.get('preferences', {}))
+            
+            return PreferencesResponse(status="success", data=res)
+        else:
+            return PreferencesResponse(status="error", error="Profile not found")
+    
+    except Exception as e:
+        return PreferencesResponse(status="error", error=f"An error occurred: {str(e)}")
     
---
+++ src/firebase/response_models/responses.py
@@ -0,0 +1,36 @@
+from typing import List, Union
+from pydantic import BaseModel
+
+
+class Alerts(BaseModel):
+    thunder: bool
+    rain: bool
+
+class Preferences(BaseModel):
+    locations: List[str]
+
+class ProfileData(BaseModel):
+    email: str
+    password: str
+    username: str
+
+class ProfileDocument(BaseModel):
+    uid: str
+    alerts: Alerts
+    preferences: Preferences
+    profile_data: ProfileData
+
+class Response(BaseModel):
+    status: str
+    data: Union[List[ProfileDocument], None] = None
+    error: Union[str, None] = None
+
+class AlertsResponse(BaseModel):
+    status: str
+    data: Alerts
+    error: Union[str, None] = None
+    
+class PreferencesResponse(BaseModel):
+    status: str
+    data: Preferences
+    error: Union[str, None] = None
