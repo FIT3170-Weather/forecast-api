0a464e37e8f5dd45f25e506119e6376b92704c56
Shyam Kamalesh Borkar <sbor0018@student.monash.edu>
2024-08-20 16:03:09 +0800

    Loaded scaler and model objects for weather prediction basead on location code

--- src/forecast/requests/requests.py
+++ src/forecast/requests/requests.py
@@ -4,6 +4,10 @@ import src.forecast.bodyParameters.locations as loc
 import src.forecast.bodyParameters.forecast_type as type
 import src.forecast.bodyParameters.variables as var
 
+# For weather forecasting model inference
+from keras.models import load_model
+import joblib
+
 router = APIRouter()
 
 """
@@ -148,28 +152,34 @@ async def getForecast(body: forecastBody):
         "success": False
     }
     
+    location_code = body.location
+    
     # Check if request body is valid
-    isValidLocation = body.location in loc.Locations().getLocations()
-    # isValidForecastType = body.forecastType in type.ForecastType().getTypes()
-    # isValidVariables = all(item in var.Variables().getVariables() for item in body.variables)
+    isValidLocation = location_code in loc.Locations().getLocations()
     
-    # TODO: Hardcoded variables returned for now, replace with models
 
-    # TODO: Filter hourly results to only return forecasts after current time
-    mock_res = {
-        "temperature": ["25.5", "26.3", "26.1", "26.0", "26.3"],
-        "humidity": ["80.1", "77.8", "77.8", "77.7", "83.0"],
-        "precipitation": ["0.0", "0.9", "1.0", "0.5", "0.8"]
-    }
-    
     # Invalid request body
-    if (not isValidLocation \
-        or not isValidVariables):
+    if (not isValidLocation):
         return res
     
+    # # TODO: Filter hourly results to only return forecasts after current time
+    # mock_res = {
+    #     "temperature": ["25.5", "26.3", "26.1", "26.0", "26.3"],
+    #     "humidity": ["80.1", "77.8", "77.8", "77.7", "83.0"],
+    #     "precipitation": ["0.0", "0.9", "1.0", "0.5", "0.8"]
+    # }
+    
+    # Use valid location to load saved forecast model according 
+    model_location = f'../../models/{location_code}/model.h5'   # Load the model from an HDF5 file
+    model = load_model(model_location)
+
+    # Load feature scalier object according to location 
+    scaler_location = f'../../../weather-forecasting/models/{location_code}/scaler.pkl'
+    scaler = joblib.load(scaler_location)
+
     # Return requested variables
-    for v in body.variables:
-        res[v] = mock_res[v]
+    # for v in body.variables:
+    #     res[v] = mock_res[v]
     res["success"] = True
     
     return res
