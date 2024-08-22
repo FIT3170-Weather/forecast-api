f9033ff3fa5758cd40f3d160d1455b90c77ef3dd
aniM <narutodaryllim43@gmail.com>
2024-08-22 16:16:26 +0800

    Merging main to firebase

--- src/forecast/bodyParameters/locations.py
+++ src/forecast/bodyParameters/locations.py
@@ -9,8 +9,9 @@ class Locations:
         self.locations = {
             "kuala-lumpur": {"lat": 3.1319, "lon": 101.6841},
             "petaling-jaya": {"lat": 3.1279, "lon": 101.5945},
-            "subang-jaya": {"lat": 3.0567, "lon": 101.5851},
-            "ipoh": {"lat": 4.6005, "lon": 101.0758}
+            "subang-jaya": {"lat": 3.057, "lon": 101.585},
+            "ipoh": {"lat": 4.6005, "lon": 101.0758},
+            "johor-bahru": {"lat": 1.4655, "lon": 103.7578}
         }
         
     def getLocations(self):
