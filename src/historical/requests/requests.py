a6a3d71ad914b41c5b2b1d6e45cff97e5515d49f
lcar0029 <lcar0029@student.monash.edu>
2024-08-22 21:17:40 +0800

    Added helper functions to aggregate hourly data by day (using average)

--- src/historical/requests/requests.py
+++ src/historical/requests/requests.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
 import requests
+import datetime as dt
 import src.forecast.bodyParameters.locations as loc
 
 router = APIRouter()
@@ -28,13 +29,47 @@ async def getCurrentWeather(body: historicalBody):
         coord = loc.Locations().getLocations()[body.location]
         lat, lon = coord["lat"], coord["lon"]
         
+        parameters = [
+            "temperature_2m",
+            "relative_humidity_2m",
+            # "precipitation",
+            # "weather_code",
+            # "cloud_cover",
+            # "visibility",
+            # "wind_speed_10m"
+        ]
+
         # Parameters
-        url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,precipitation,weather_code,cloud_cover,visibility,wind_speed_10m,wind_speed_80m,wind_speed_120m,wind_speed_180m,temperature_80m,temperature_120m,temperature_180m&past_days=7&forecast_days=1"
+        url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&hourly={','.join(parameters)}&past_days=2&forecast_days=0"
 
         res = requests.get(url)
         if res.status_code == 200:
-            res = res.json()    
+            res = res.json()
+            uniqueDates = getUniqueDates(res["hourly"]["time"])
+            
+            new = {}    
+            for i in range(len(uniqueDates)):
+                new[uniqueDates[i]] = {}
+                for parameter in parameters:   
+                    today = res["hourly"][parameter][i*24:(i*24)+24]
+                    avg = averageDay(today)
+                    # print(len(today), today)
+                    new[uniqueDates[i]][parameter] = avg
+            
+            return new
             return res
-    
+
     return {"success": False}
+
+def getUniqueDates(datetimes):
+    dates = []
+    format = "%Y-%m-%dT%H:%M"
+    for date in datetimes:
+        date = (dt.datetime.strptime(date, format)).date()
+        if (date not in dates):
+            dates.append(date)
+    return dates
+
+def averageDay(variables):
+    return sum(variables)/len(variables)
 
