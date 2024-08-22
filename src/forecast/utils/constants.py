e802425c15c1820b3b94a3f508fbb4643c6cb889
Shyam Kamalesh Borkar <sbor0018@student.monash.edu>
2024-08-22 15:41:58 +0800

    Added new constants for precipitation decimal points

--- src/forecast/utils/constants.py
+++ src/forecast/utils/constants.py
@@ -1,7 +1,9 @@
 # Description: Constants used in the forecast module
 
+DAILY_HOURS = 24
 FORECAST_PREV_DAYS = 3 # Past 3 days for inference
 FORECAST_CURRENT_DAYS = 1 # This means the current day also used in inference
 DAYS_TO_FORECAST = 7
+PRECIPITATION_DP = 1
 TIMEZONE = 'Asia/Singapore'
 PREDICTED_ATTRIBUTES = ['temperature_2m', 'relative_humidity_2m', 'dew_point_2m', 'rain', 'pressure_msl', 'wind_speed_10m', 'wind_direction_10m']
